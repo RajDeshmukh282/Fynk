b0a739c30055de02e046fef14bb5df40efd58fe0
Raj Deshmukh <rajdeshmukh282@gmail.com>
2025-09-06 23:43:25 +0530

    Add header element to App component and adjust imports for routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
-import Router from './Router'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <>
-
       <Routes>
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/SignIn" element={<SignIn />} />
       </Routes>
     </>
-    
-  )
+  );
 }
 
-export default App
+export default App;
