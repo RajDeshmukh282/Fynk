f448dc8fbbe5a3c487c215d2ba59f9d687ae86b0
Raj Deshmukh <rajdeshmukh282@gmail.com>
2025-09-07 12:17:48 +0530

    Refactor SignUp component for improved readability and consistency

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const SignUp = () => {
   return (
     <>
-    <div className='w-full h-screen bg-gradient-to-b
- from-black to-gray-900 flex flex-col justify-center items-center'>
+      <div
+        className="w-full h-screen bg-gradient-to-b
+ from-black to-gray-900 flex flex-col justify-center items-center"
+      >
+        <div class="w-[90%] lg:max-w-[60%] h-[600px] bg-white rounded-2xl flex justify-center items-center overflow-hidden border-2 border-[#1a1f23]">
+          <div class="w-full lg:w-[50%] h-full bg-white flex flex-col items-center p-[10px] gap-[20px]"></div>
 
-
-    <div class="w-[90%] lg:max-w-[60%] h-[600px] bg-white rounded-2xl flex justify-center items-center overflow-hidden border-2 border-[#1a1f23]">
-  </div>
-  
-  </div>
-
+          <div class="md:w-[50%] h-full hidden lg:flex justify-center items-center bg-[#000000] flex-col gap-[10px] text-white text-[16px] font-semibold rounded-l-[30px] shadow-2xl shadow-black"></div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
